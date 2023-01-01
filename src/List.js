8273a1523554881c0617191dc82a91737e4de19b
Lewis Benson <tm.lewisbenson@gmail.com>
2022-12-31 22:06:23 -0500

    add state hook and list of people to application

--- src/List.js
+++ src/List.js
@@ -1,9 +1,28 @@
+/** @format */
+
 import React from 'react';
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article
+            key={id}
+            className='person'
+          >
+            <div>
+              <img
+                src={image}
+                alt={name}
+              />
+              <h4>{name}</h4>
+              <p>{age} years old</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,5 +9,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
